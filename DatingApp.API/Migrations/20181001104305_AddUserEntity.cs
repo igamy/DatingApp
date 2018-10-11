ef45cad240ba3a9c74aaf58c396caa95b31e2d89
unknown <ivo.gamito@tecnico.ulisboa.pt>
2018-10-11 14:38:47 +0100

    End of Chapter 3

---
+++ DatingApp.API/Migrations/20181001104305_AddUserEntity.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DatingApp.API.Migrations
+{
+    public partial class AddUserEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            /*migrationBuilder.DropPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty");*/
+
+            /*migrationBuilder.RenameTable(
+                name: "MyProperty",
+                newName: "Values");*/
+
+            /*migrationBuilder.AddPrimaryKey(
+                name: "PK_Values",
+                table: "Values",
+                column: "Id");*/
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Username = table.Column<string>(nullable: true),
+                    PasswordHash = table.Column<byte[]>(nullable: true),
+                    PasswordSalt = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Values",
+                table: "Values");
+
+            /*migrationBuilder.RenameTable(
+                name: "Values",
+                newName: "MyProperty");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_MyProperty",
+                table: "MyProperty",
+                column: "Id");*/
+        }
+    }
+}
