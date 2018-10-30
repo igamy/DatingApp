3e1b833106460e50d745e34aef54506b95b51ebb
unknown <ivo.gamito@tecnico.ulisboa.pt>
2018-10-30 12:00:13 +0000

    End of Section 9

--- DatingApp.API/helpers/AutoMapperProfiles.cs
+++ DatingApp.API/helpers/AutoMapperProfiles.cs
@@ -19,6 +19,9 @@ namespace DatingApp.API.helpers
             CreateMap<User, UserForDetailedDto>()
                 .ForMember(dest => dest.PhotoUrl, opt => {
                     opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url);
+                })
+                 .ForMember(dest => dest.Age, opt => {
+                    opt.ResolveUsing(d => d.DateOfBirth.CalculateAge());
                 });
             CreateMap<Photo, PhotosForDetailedDto>();
         }
